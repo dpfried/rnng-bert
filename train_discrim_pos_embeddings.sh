c657e0877e67f3e74a642f09e686e5776f0e9727
Daniel Fried <dfried@email.arizona.edu>
2016-11-15 12:52:49 -0800

    add pos and embedding training scripts, and no stack

---
+++ train_discrim_pos_embeddings.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+output_path=$1
+build/nt-parser/nt-parser --cnn-mem 1700 -x -T corpora/train.oracle -d corpora/dev.oracle -C corpora/dev.stripped -t --pretrained_dim 300 -w embeddings/GoogleNews-ptb_filtered-vectors-negative300.txt -P --lstm_input_dim 128 --hidden_dim 128 -D 0.2 > ${output_path}.stdout 2> ${output_path}.stderr
