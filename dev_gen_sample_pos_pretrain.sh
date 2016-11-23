9f13de0a3f62b95e0648c4df26e502ecff4e1a2c
Daniel Fried <dfried@email.arizona.edu>
2016-11-23 03:52:27 +0000

    fix sampling, add pos pretrain sampl

--- dev_gen_sample_pos_pretrain.sh
+++ dev_gen_sample_pos_pretrain.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 num_samples=$1
-discrim_model="ntparse_0_2_32_128_16_128-pid8583.params"
+discrim_model="ntparse_pos_pretrained_0_2_32_128_16_128-pid6846.params"
 gen_model="ntparse_gen_D0.3_2_256_256_16_256-pid4187.params"
 num_sentences=1700 # dev
 
-samples=expts/dev_s=${num_samples}.samples
+samples=expts/dev_pos_embeddings_s=${num_samples}.samples
 
 trees=${samples}.trees
 likelihoods=${samples}.likelihoods
@@ -16,7 +16,7 @@ hyp=${samples}.hyp.trees
 hyp_final=${samples}.hyp_final.trees
 parsing_result=${samples}.parsing_result.txt
 
-build/nt-parser/nt-parser --cnn-mem 1700 -x -T corpora/train.oracle -p corpora/dev.oracle -C corpora/dev.stripped --lstm_input_dim 128 --hidden_dim 128 -m $discrim_model --alpha 0.8 -s $num_samples > $samples
+build/nt-parser/nt-parser --cnn-mem 1700 -x -T corpora/train.oracle -p corpora/dev.oracle -C corpora/dev.stripped --pretrained_dim 300 -w embeddings/GoogleNews-ptb_filtered-vectors-negative300.txt -P --lstm_input_dim 128 --hidden_dim 128 -m $discrim_model --alpha 0.8 -s $num_samples > $samples
 utils/cut-corpus.pl 3 $samples > $trees
 build/nt-parser/nt-parser-gen -x -T corpora/train_gen.oracle --clusters clusters-train-berk.txt --input_dim 256 --lstm_input_dim 256 --hidden_dim 256 -p $trees -m $gen_model > $likelihoods
 utils/is-estimate-marginal-llh.pl $num_sentences $num_samples $samples $likelihoods > $log_likelihoods 2> $rescored
