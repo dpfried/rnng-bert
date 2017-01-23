cd331efaadae46f08fc5369ce9adc61e72b2a0a4
Daniel Fried <dfried@berkeley.edu>
2017-01-23 09:23:47 +0000

    also load silver oracle

--- dev_discrim_pos_embeddings_ling_silver_seed.sh
+++ dev_discrim_pos_embeddings_ling_silver_seed.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 build/nt-parser/nt-parser \
-    --cnn-mem 3000,3000,4000 \
+    --cnn-mem 3000,0,4000 \
     -x \
     -T corpora/silver_train.oracle \
     -p corpora/silver_dev.oracle \
     -C corpora/dev.stripped \
+    --gold_training_data corpora/silver_wsj-train.oracle \
     -P
     --pretrained_dim 100 \
-    -w embeddings/GoogleNews-ptb_filtered-vectors-negative300.txt \
+    -w embeddings/sskip.100.vectors \
     --lstm_input_dim 128 \
     --hidden_dim 128 \
     -m $1
