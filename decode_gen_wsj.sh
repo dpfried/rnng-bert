1236c8510faff899932c828ce76fa25a1048729d
Daniel Fried <dfried@berkeley.edu>
2017-02-04 13:14:29 -0800

    updated decode script from gen_beam

--- decode_gen_wsj.sh
+++ decode_gen_wsj.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# epoch 82, perplexity = 107.532
-model_file=ntparse_gen_D0.3_2_256_256_16_256-seed4-pid1478.params.bin
+# epoch 121.761, perplexity = 106.772
+model_file=ntparse_gen_D0.3_2_256_256_16_256-seed3-pid1475.params.bin
 
 if [ "$#" -ne 3 ]; then
     echo "args: beam_size beam_size_at_word block_num"
@@ -14,11 +14,12 @@ block_num=$3
 block_count=100
 prefix="rnng_gen-beam_size=${beam_size}-at_word=${at_word}"
 decode_output=expts/${prefix}.decode # don't need block num, the code will append it
+decode_beam=expts/${prefix}.beam # don't need block num, the code will append it
 stdout=expts/${prefix}.stdout_block-${block_num}
 stderr=expts/${prefix}.stderr_block-${block_num}
 
 build/nt-parser/nt-parser-gen \
-    --cnn-mem 25000,0,500 \
+    --cnn-mem 50000,0,500 \
     -T corpora/train_gen.oracle \
     -d corpora/dev_gen.oracle \
     -C corpora/dev.stripped \
@@ -33,6 +34,7 @@ build/nt-parser/nt-parser-gen \
     --decode_beam_size $beam_size \
     --decode_beam_filter_at_word_size $at_word \
     --dev_output_file $decode_output \
+    --dev_beam_file $decode_beam \
     --block_count $block_count \
     --block_num $block_num \
     > $stdout \
