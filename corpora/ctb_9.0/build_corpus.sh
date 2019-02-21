60de782f5eb690d84d453e510feca9c38c413099
Daniel Fried <dfried@berkeley.edu>
2019-02-20 18:07:58 -0800

    CTB 9.0 processing (untested)

--- corpora/ctb_9.0/build_corpus.sh
+++ corpora/ctb_9.0/build_corpus.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-TRAIN="train.gold.original"
-DEV="dev.gold.original"
-TEST="test.gold.original"
-OTHERS="others.gold.original"
+source ../analysis_path.sh
+
 
 SCRIPT_DIR="../../scripts"
 
@@ -15,38 +13,46 @@ GET_ORACLE_GEN=${SCRIPT_DIR}/get_oracle_gen.py
 NORMALIZE_CHINESE=${SCRIPT_DIR}/normalize_chinese_punct.py
 NORMALIZE_UNICODE=${SCRIPT_DIR}/normalize_unicode.py
 REPLACE_SYMBOLS=${SCRIPT_DIR}/replace_symbols.py
+STRIP_ROOT=${SCRIPT_DIR}/strip_root.py
 
 BERT_PATH="../../bert_models/chinese_L-12_H-768_A-12/"
 
-python $REMOVE_TRACES < $TRAIN | python $NORMALIZE_CHINESE | python $NORMALIZE_UNICODE > train.gold.normed.stripped
-python $REMOVE_TRACES < $DEV | python $NORMALIZE_CHINESE | python $NORMALIZE_UNICODE > dev.gold.normed.stripped
-python $REMOVE_TRACES < $TEST | python $NORMALIZE_CHINESE | python $NORMALIZE_UNICODE > test.gold.normed.stripped
-python $FILTER_BAD < $OTHERS | python $REMOVE_TRACES | python $NORMALIZE_CHINESE  | python $NORMALIZE_UNICODE > others.gold.normed.stripped.all-tags
-#python $FILTER_BAD < $OTHERS | python $REMOVE_TRACES | python $NORMALIZE_CHINESE > others.gold.normed.stripped.all-tags
-# important! don't evaluate against others.gold.stripped, just use it for oracle
-python $REPLACE_SYMBOLS --map_from DFL EMO FLR IMG INC META OTH SKIP TYPO WHPP --map_to FRAG < others.gold.normed.stripped.all-tags > others.gold.normed.stripped
+for SPLIT in train dev test
+do
+  STRIPPED=${SPLIT}.pred.stripped
+  ln -s $ANALYSIS_DIR/corpora/ctb_9.0/$STRIPPED $STRIPPED
+  PROCESSED=${SPLIT}.pred.processed
 
-DICTIONARY="train.normed.dictionary"
+  python $STRIP_ROOT --symbol "TOP" --must_have < $STRIPPED > $PROCESSED
+done
 
-python $GET_DICTIONARY train.gold.normed.stripped > $DICTIONARY
+DICTIONARY="train.out-domain.dictionary"
+rm $DICTIONARY
+ln -s ../ctb_5.1/train.dictionary $DICTIONARY
 
 mkdir top_down 2> /dev/null
 mkdir in_order 2> /dev/null
 
-# discriminative oracles
-for SPLIT in train.gold.normed dev.gold.normed test.gold.normed others.gold.normed
+for SPLIT in newswire broadcast_news broadcast_conversations weblogs discussion_forums chat_messages conversational_speech
 do
   echo "split: " $SPLIT
+  STRIPPED=${SPLIT}.pred.stripped
+  ln -s $ANALYSIS_DIR/corpora/ctb_9.0/$STRIPPED $STRIPPED
+
+  PROCESSED=${SPLIT}.pred.processed
+  python $REPLACE_SYMBOLS --map_from DFL EMO FLR IMG INC META OTH SKIP TYPO WHPP --map_to FRAG < $STRIPPED \
+    | python $STRIP_ROOT --symbol "TOP" --must_have > $PROCESSED
+
   # in_order discriminative
-  python $GET_ORACLE --in_order $DICTIONARY ${SPLIT}.stripped --no_morph_aware_unking --bert_model_dir $BERT_PATH > in_order/${SPLIT}.oracle
-  python $GET_ORACLE --in_order $DICTIONARY ${SPLIT}.stripped --no_morph_aware_unking --bert_model_dir $BERT_PATH --collapse_unary > in_order/${SPLIT}.collapse-unary.oracle
-  python $GET_ORACLE --in_order $DICTIONARY ${SPLIT}.stripped --no_morph_aware_unking --bert_model_dir $BERT_PATH --reverse_trees > in_order/${SPLIT}.reverse-trees.oracle
+  python $GET_ORACLE --in_order $DICTIONARY $PROCESSED --no_morph_aware_unking --bert_model_dir $BERT_PATH > in_order/${SPLIT}.pred.oracle
+  # python $GET_ORACLE --in_order $DICTIONARY $PROCESSED --no_morph_aware_unking --bert_model_dir $BERT_PATH --collapse_unary > in_order/${SPLIT}.pred.collapse-unary.oracle
+  # python $GET_ORACLE --in_order $DICTIONARY $PROCESSED --no_morph_aware_unking --bert_model_dir $BERT_PATH --reverse_trees > in_order/${SPLIT}.pred.reverse-trees.oracle
 
   # top_down discriminative
-  python $GET_ORACLE $DICTIONARY ${SPLIT}.stripped --no_morph_aware_unking --bert_model_dir $BERT_PATH > top_down/${SPLIT}.oracle
-  python $GET_ORACLE $DICTIONARY ${SPLIT}.stripped --no_morph_aware_unking --bert_model_dir $BERT_PATH --collapse_unary > top_down/${SPLIT}.collapse-unary.oracle
-  python $GET_ORACLE $DICTIONARY ${SPLIT}.stripped --no_morph_aware_unking --bert_model_dir $BERT_PATH --reverse_trees > top_down/${SPLIT}.reverse-trees.oracle
+  python $GET_ORACLE $DICTIONARY $PROCESSED --no_morph_aware_unking --bert_model_dir $BERT_PATH > top_down/${SPLIT}.pred.oracle
+  # python $GET_ORACLE $DICTIONARY $PROCESSED --no_morph_aware_unking --bert_model_dir $BERT_PATH --collapse_unary > top_down/${SPLIT}.pred.collapse-unary.oracle
+  # python $GET_ORACLE $DICTIONARY $PROCESSED --no_morph_aware_unking --bert_model_dir $BERT_PATH --reverse_trees > top_down/${SPLIT}.pred.reverse-trees.oracle
 
   # top_down generative
-  python $GET_ORACLE_GEN $DICTIONARY ${SPLIT}.stripped --no_morph_aware_unking > top_down/${SPLIT}_gen.oracle
+  python $GET_ORACLE_GEN $DICTIONARY $PROCESSED --no_morph_aware_unking > top_down/${SPLIT}_gen.pred.oracle
 done
