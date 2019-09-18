2680f6cc4ddfa8ab550038009c2d398dc9763964
Daniel Fried <dfried@berkeley.edu>
2019-09-17 21:42:28 -0700

    Update decode script

--- bert_decode_from_tokens.sh
+++ bert_decode_from_tokens.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
-corpus=$1
-block_number=$2
+sentsplit_token_file=$1
+working_dir=$2
+
+output_file="${sentsplit_token_file}.parsed"
+
+echo "will write output file to $output_file"
+
+dictionary_file="corpora/english.train.dictionary"
+model_dir="models/uncased-english-wwm"
+
+train_oracle_file="corpora/english/in_order/train.oracle"
+
+if [ -z $working_dir ]
+then
+        working_dir=`mktemp -d`
+        echo "writing temporary files in $working_dir"
+fi
+
+oracle_file="${working_dir}/`basename $sentsplit_token_file`.oracle"
+sentsplit_decode_file="${working_dir}/`basename $sentsplit_token_file`.sentsplit-decodes"
+long_sentences_file="${working_dir}/`basename $sentsplit_token_file`.long-sents"
+
+python3 scripts/get_oracle.py \
+        corpora/english/train.dictionary \
+        $sentsplit_token_file  \
+        --is_token_file \
+        --bert_model_dir $model_dir \
+        --base_fname $oracle_file \
+        --long_sentences_output_file $long_sentences_file
+
 build/nt-parser/nt-parser \
         --cnn-seed 1 \
         --cnn-mem 3000,0,500 \
-        --model_dir models/uncased-english-wwm \
-        -T corpora/english/in_order/train.oracle \
-        --test_data ../../corpora/oracles/${corpus}_raw_subsampled_tokenized_sentsplit.token-oracle-block-$block_number \
+        --model_dir $model_dir \
+        -T $train_oracle_file \
+        --test_data $oracle_file \
         --test_data_no_trees \
         --lstm_input_dim 128 \
         --hidden_dim 128 \
@@ -16,7 +44,17 @@ build/nt-parser/nt-parser \
         --inorder \
         --bert \
         --bert_large \
-        --ptb_output_file ../../corpora/decodes/${corpus}_raw_subsampled_tokenized_sentsplit.tagged-decode-block-$block_number \
+        --ptb_output_file $sentsplit_decode_file \
         --output_trees_only \
         --output_beam_as_samples \
-        > ../../corpora/decodes/${corpus}_raw_subsampled_tokenized_sentsplit.decode-block-$block_number
+        > /dev/null
+
+python3 scripts/cat_decodes.py \
+        $sentsplit_token_file \
+        $sentsplit_decode_file \
+        $output_file \
+        --bert_model_dir $model_dir \
+        --long_sentences_file $long_sentences_file
+
+echo `wc -l < $sentsplit_token_file` lines in input file, $sentsplit_token_file
+echo `wc -l < $output_file` lines in output file, $output_file
